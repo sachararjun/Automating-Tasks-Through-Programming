f0c68fe7c63fba3f1b482e5cc513e50de09684d8
vikas kumar <55294793+sachararjun@users.noreply.github.com>
2020-04-27 14:03:55 +0530

    health_checks and network are added.

---
+++ health_checks.py
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html  lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+  <title>
+    Qwiklabs Support 
+  </title>
+  <link rel='shortcut icon' href='/hc/favicon' type='image/png'/>
+  
+  <link href="https://qwiklab.zendesk.com/access/unauthenticated" rel="canonical"></link>
+  <meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="znLMXRq+fCw1zI66HHj/AVIvabWCIkwakuo9WyftdcrMd5Ioq06gNo+f2VnmxCYF" />
+  <link rel="stylesheet" media="screen" href="https://static.zdassets.com/classic/assets/application/help_center-7ba24c6033ea7cf1e2f71acea100816d.css" />
+
+  <script src="https://static.zdassets.com/classic/assets/vendor/jquery-1.8.3.min-18dcbff61bcf3284725bc3681846d1de.js"></script>
+  <!--[if gte IE 7]>
+    <script src="https://static.zdassets.com/classic/assets/Placeholders-6beed63190371310ca5647a32b015cc9.js"></script>
+  <![endif]-->
+  <script src="https://static.zdassets.com/classic/assets/help_center-06717c1b512ec8a36adbb27f55eb03b4.js"></script>
+  
+</head>
+
+<body class="access access-unauthenticated">
+  
+
+  <div class="header">
+    <div class="header_inner">
+      <div class="logo">
+        <a href="/hc" title="<span class=&quot;translation_missing&quot; title=&quot;translation missing: en-US-x-1.txt.help_center.helpers.application.home&quot;>Home</span>"><img id="logo" alt="Logo" src="/hc/logo" /></a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="layout_wrap">
+    <div class="layout_inner">
+      <script src="https://static.zdassets.com/classic/assets/zendesk/auth/v2/host-a187d22ebca5d23bbde57637392a23b0.js" data-brand-id="2296516" data-auth-origin="2296516,false,true" data-auth-domain="https://qwiklab.zendesk.com" data-return-to="https://qwiklab.zendesk.com/attachments/token/LOp91VZj6FL75zWaAFMe5zUeq?name=health_checks+%281%29+%281%29+%282%29.py" data-theme="hc" data-locale="1" data-action="signin"></script>
+
+    </div>
+    
+  </div>
+
+</body>
+</html>
